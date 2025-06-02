cb0be1d5c505f0ea4ccefe8820f974f32cd058c5
OSCAR NÚÑEZ MORI <oscar.catedra@gmail.com>
2025-06-02 18:51:59 -0500

    Add files via upload

---
+++ 04_C/01_C/01_hola_mundo/hola_mundo.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("Hola Mundo\n");
+    return 0;
+}
+
+// https://chatgpt.com/share/67f6b4cb-0b00-800a-a1e4-4c80ddc8453d
+
+// gcc hola_mundo.c -o hola_mundo.exe
+
+
