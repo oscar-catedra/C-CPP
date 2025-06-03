daf2a7763383deb84b359ddd41316397f9410ab9
OSCAR NÚÑEZ MORI <oscar.catedra@gmail.com>
2025-06-02 20:15:28 -0500

    Add files via upload

---
+++ 05_CPP/01_CPP/01_hola_mundo/hola_mundo.cc
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hola Mundo" << std::endl;
+    return 0;
+}
+
+// Referencia
+//
+// OpenAI. (2025, abril 9). Ejemplos en Lenguaje C y C++ [Chat]. ChatGPT. 
+// https://www.openai.com/chatgpt
+
+// g++ hola_mundo.cc -o hola_mundo.exe
+//
+
